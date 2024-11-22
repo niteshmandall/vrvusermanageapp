25f6a5963a880df5948719f389d7cdbb3a365f89
kashyapanjali <anjalikashyap9608@gmail.com>
2024-11-22 20:02:47 +0530

    route add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserManageMent from "./component/UserManageMent";
 import LoginSignUp from "./component/LoginSignUp";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <LoginSignUp />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginSignUp />} />
+          <Route path="/dashboard" element={<Navbar />} />
+        </Routes>
+        <UserManageMent />
+      </div>
+    </Router>
   );
 }
 
